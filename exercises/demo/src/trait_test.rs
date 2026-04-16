c534f18c509b3bb17b8f21b36b24403dae4198df
wuxin <myeagleandsnake@gaml.com>
2026-04-17 00:52:13 +0800

    feat: test

--- exercises/demo/src/trait_test.rs
+++ exercises/demo/src/trait_test.rs
@@ -2,14 +2,84 @@ fn main() {
 
 }
 
+#[test]
+fn test_r_some(){
+    let  color_show  = ColorShow::new();
+    test_t_som(&color_show);
+    test_dyn(&color_show);
+    let breaking_bak_tv = BreakingBad::new();
+    breaking_bak_tv.show_tv("1");
+    breaking_bak_tv.show_color("蓝绿色")
+
+
+
+}
+
 
 
 pub trait Display {
     fn show_color(&self, c: &str) -> ();
 }
 
-impl show_color for Display  {
+
+fn test_dyn(d: &dyn Display) -> (){
+
+    d.show_color("black");
+
+}
+fn test_t_som<T: Display> (t: &T) -> (){
+    t.show_color("red");
+}
+
+
+struct ColorShow {}
+
+impl Display for ColorShow  {
     fn show_color(&self, c : &str) -> () {
         println!("现在的颜色是:{}", c);
+    }
+}
+
+
+pub trait TvShow{
+    fn show_tv(&self,s: &str) where Self: Sized;
+}
+
+impl <T: TvShow> Display for T {
+    fn show_color(&self, c: &str) -> () {
+        println!("电视的主题色是{}", c);
+    }
+
+
+}
+
+
+impl ColorShow {
+    fn new() -> Self {
+        Self{
+
+        }
+    }
+}
+
+struct BreakingBad{
+
+
+}
+
+
+impl BreakingBad{
+    fn new() -> Self {
+        Self{}
+    }
+}
+
+
+impl TvShow for BreakingBad{
+    fn show_tv(&self,s: &str)
+    where
+        Self: Sized
+    {
+        println!(" 现在播放绝命毒师第{}集", s);
     }
 }
