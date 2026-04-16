877ed2b8a6396155e913cc0b5c3431d40d6534da
wuxin <myeagleandsnake@gaml.com>
2026-04-16 23:56:48 +0800

    feat: test

--- exercises/demo/src/enums_test.rs
+++ exercises/demo/src/enums_test.rs
@@ -1,4 +1,6 @@
+use std::any::Any;
 use std::fmt::{Display, Formatter};
+use crate::Color::Red;
 
 fn main() {
     let  a = IpAddr::V4(1,2,4,5);
@@ -8,12 +10,35 @@ fn main() {
     let e: Option<u32>  = Some(5);
     println!("{}", e.expect("error"));
     println!("{}", e.unwrap_or(0));
-    let f: Option<u32> = None;
-    let eu = f.expect("exception----------->");
-    println!("{}", eu);
+    // let f: Option<u32> = None;
+    // let eu = f.expect("exception----------->");
+    // println!("{}", eu);
+}
+
+#[test]
+fn  test_value_color(){
+    let a  = value_in_color(Color::Blue(1));
+    println!("{:?}", a);
 }
 
 
+#[test]
+fn let_ele_test (){
+   let a   = Some(1);
+    if let Some(b) = a {
+        println!("{}", b);
+    }
+
+}
+
+fn value_in_color(need_match: Color) -> Color{
+    match need_match {
+        Red(_) => Red("aa".to_string()),
+        other => other
+    }
+}
+
+#[derive(Debug)]
 enum Color {
     Red(String),
     Green(u8),
